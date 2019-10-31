c173440a509384b927a41be03952a97745e646b6
HelloQA <helloqa46@gmail.com>
2019-10-31 11:38:43 +0530

    created a display function

--- Framework/src/test/HelloWorld.java
+++ Framework/src/test/HelloWorld.java
@@ -2,6 +2,14 @@ package test;
 
 public class HelloWorld {
 
+	public void display() {
+		
+		System.out.println("Hello");
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
